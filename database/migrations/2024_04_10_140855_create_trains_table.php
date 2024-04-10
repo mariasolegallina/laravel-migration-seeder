e8b5f98f83fb188e18380e4b11101378cb753169
mariasolegallina <gallinamariasole@gmail.com>
2024-04-10 16:38:44 +0200

    add tables

--- database/migrations/2024_04_10_140855_create_trains_table.php
+++ database/migrations/2024_04_10_140855_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code');
+            $table->tinyInteger('number_of_wagons')->unsigned();
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
